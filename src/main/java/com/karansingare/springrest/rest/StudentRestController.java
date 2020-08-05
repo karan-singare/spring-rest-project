2b65a7cb12d2f158f4e812643dc6b2dd90b23bbb
Karan Singh Singare <Karan Singh Singare>
2020-08-05 14:07:55 +0530

    added the custom exception handler for the student

--- src/main/java/com/karansingare/springrest/rest/StudentRestController.java
+++ src/main/java/com/karansingare/springrest/rest/StudentRestController.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,12 +33,41 @@ public class StudentRestController {
 		
 	}
 	
-	// define ednpoint for "/students" return list of students
+	// define endpoint for "/students" return list of students
 	@GetMapping("/students")
 	public List<Student> getStudents() {
 		return theStudents;
 	}
 	
-	// 
+	// define endpoint for "/student/{studentId}" -  return student at index
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		//just index into to the list
+		
+		// check the StudentId against the list size
+		
+		if (studentId >= theStudents.size() || studentId < 0) {
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
+		
+		return theStudents.get(studentId);
+	}
+	
+	// Add an exception handler using @ExceptionHandler
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+		// create a StudentErrorREsponse 
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		// return ResponseEntity
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+	
 	
 }
